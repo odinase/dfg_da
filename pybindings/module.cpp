a1a02381ae025217d89bf5c24ca0a5b6f3757467
Odin <odin.sev@gmail.com>
2022-10-04 12:00:03 +0200

    Start of adding pybind pieces

---
+++ pybindings/module.cpp
@@ -0,0 +1,45 @@
+#include <pybind11/pybind11.h>
+#include <eigen3/Eigen/Core>
+#include <pybind11/eigen.h>
+#include <pybind11/stl.h>
+#include <memory>
+#include "af_target_simulator/Test.h"
+#include "af_target_simulator/types.h"
+#include "af_target_simulator/targets/Target.h"
+#include "af_target_simulator/dynamics/DynamicModel.h"
+#include "wrappers/wrappers.h"
+
+using namespace af_target_simulator;
+using namespace targets;
+using namespace dynamics;
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(af_target_simulator, m)
+{
+    // m.doc() = "pybind11 example plugin"; // optional module docstring
+    py::class_<Test>(m, "Test")
+        .def(py::init<>());
+    m.def("test_optional", &test_optional);
+    // m.def("test_shared_ptr", &test_shared_ptr);
+    m.def("test_ptr", &test_ptr);
+    m.def("test_vec", &test_vec);
+
+
+    // .def("setName", &Pet::setName)
+    // .def("getName", &Pet::getName);
+    py::module_ targets = m.def_submodule("targets");
+    py::class_<Target>(targets, "Target")
+        .def(py::init<const VectorConstRef, std::shared_ptr<DynamicModel>, int>(), py::arg("init_state"), py::arg("dyn_model"), py::arg("len_story") = 1);
+
+    py::module_ dynamics = m.def_submodule("dynamics");
+    py::class_<DynamicModel, std::shared_ptr<DynamicModel>, PyDynamicModel>(dynamics, "DynamicModel")
+        .def(py::init<>())
+        .def("state_space_dim", &DynamicModel::state_space_dim)
+        .def("pos_idx", &DynamicModel::pos_idx)
+        .def("input_vec", &DynamicModel::input_vec)
+        .def("gain_mat", &DynamicModel::gain_mat)
+        .def("cov_mat", &DynamicModel::cov_mat)
+        .def("dyn_mat", &DynamicModel::dyn_mat)
+        .def("propagate", &DynamicModel::propagate);
+}
