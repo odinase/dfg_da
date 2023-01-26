Fixed bug in returning normalization constants and added plotting comparing Odin to LC

dfg_da/marginals_computers.py
@@ -130,22 +130,24 @@ class LBPMarginalsByTotalProbBethe(MarginalsComputer):
 
         conditioned_marginals = np.empty((n, m + 2))
 
-        normalizing_constants = np.empty(len(prior_hypotheses))
-
+        normalizing_constants_odin = np.empty(len(prior_hypotheses))
+        normalizing_constants_lc = np.empty(len(prior_hypotheses))
+    
         for k, (tracks, hypo_prob) in enumerate(prior_hypotheses):
 
             R_sub = R_LC[tracks-1, :]
 
             if len(tracks) > 0:
-                lbp_probs, it_from_lbp, converged, bethe_log, mu, nu, w_nmd = lbp_marginal(R_sub, return_mu_nu_w_nmd=True)
+                lbp_probs, it_from_lbp, converged, bethe_log_lc, mu, nu, w_nmd = lbp_marginal(R_sub, return_mu_nu_w_nmd=True)
                 F_b_psuedo = self.bethe_constant(w_nmd, mu, nu)
-                bethe_log = -F_b_psuedo
+                bethe_log_odin = -F_b_psuedo
             else:
                 # Williams LBP returns wonky stuff for empty hypotheses, set sepcific values
                 lbp_probs = np.empty((0, R_LC.shape[1]))
                 it_from_lbp = 0
                 converged = True
-                bethe_log = 0
+                bethe_log_lc = 0
+                bethe_log_odin = 0
             
             # We need to concatenate the JPDAprobs with all tracks and existence probs
             existing_tracks_idx = tracks - 1
@@ -157,7 +159,9 @@ class LBPMarginalsByTotalProbBethe(MarginalsComputer):
             conditioned_marginals[existing_tracks_idx] = existing_probs
             conditioned_marginals[non_existing_tracks_idx] = nonexisting_probs
 
-            normalizing_constant = np.exp(bethe_log) # self.bethe_constant(mu, nu, w_nmd)
+            normalizing_constant = np.exp(bethe_log_odin) # self.bethe_constant(mu, nu, w_nmd)
+            normalizing_constants_odin[k] = normalizing_constant
+            normalizing_constants_lc[k] = np.exp(bethe_log_lc)
 
             lbp_marginal_total += conditioned_marginals * normalizing_constant * hypo_prob
 
@@ -167,7 +171,7 @@ class LBPMarginalsByTotalProbBethe(MarginalsComputer):
         assert (np.abs(lbp_marginal_total.sum(axis=1) - 1.0) < 1e-6).all()
         assert ((0 <= lbp_marginal_total) & (lbp_marginal_total <= 1.0)).all()
 
-        return lbp_marginal_total, normalizing_constants
+        return lbp_marginal_total, normalizing_constants_odin, normalizing_constants_lc
 
 
 

plotting.py
@@ -698,22 +698,26 @@ def compare_converge_not_converge(cluster_stats: List[Tuple[ClusterData, Path]])
 
 def normalization_constant_scatter_plot(cluster_stats: List[Tuple[ClusterData, Path]]):
     phd_normalization_constants = []
-    bethe_normalization_constants = []
+    bethe_normalization_constants_odin = []
+    bethe_normalization_constants_lc = []
     exact_normalization_constants = []
 
     for cluster_stat, cluster_file in cluster_stats:
         if not cluster_stat.explicit_hypothesis_enumeration_error:
             phd_normalization_constants.append(cluster_stat.williams_stats.normalization_constants)
             exact_normalization_constants.append(cluster_stat.exact_stats.normalization_constants)
-            bethe_normalization_constants.append(cluster_stat.bethe_stats.normalization_constants)
+            bethe_normalization_constants_odin.append(cluster_stat.bethe_stats.normalization_constants_odin)
+            bethe_normalization_constants_lc.append(cluster_stat.bethe_stats.normalization_constants_lc)
 
     phd_normalization_constants = np.hstack(phd_normalization_constants)
-    bethe_normalization_constants = np.hstack(bethe_normalization_constants)
+    bethe_normalization_constants_lc = np.hstack(bethe_normalization_constants_lc)
+    bethe_normalization_constants_odin = np.hstack(bethe_normalization_constants_odin)
     exact_normalization_constants = np.hstack(exact_normalization_constants)
 
     fig, ax = plt.subplots()
-    ax.plot(phd_normalization_constants, exact_normalization_constants, 'o', alpha=0.3, label="PHD")
-    ax.plot(bethe_normalization_constants, exact_normalization_constants, 'o', alpha=0.3, label="Bethe")
+    ax.plot(phd_normalization_constants, exact_normalization_constants, 'o', alpha=0.2, label="PHD")
+    ax.plot(bethe_normalization_constants_odin, exact_normalization_constants, 'o', alpha=0.2, label="Bethe Odin")
+    ax.plot(bethe_normalization_constants_lc, exact_normalization_constants, 'o', alpha=0.2, label="Bethe LC")
     ax.plot(exact_normalization_constants, exact_normalization_constants, '--', label="Perfect correlation")
     ax.set_xlabel("Approximate normalization constant")
     ax.set_ylabel("Exact normalization constant")