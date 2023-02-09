a3d40ffd0ff5016b509784975d78d9aac34a09ad
Odin <odin.sev@gmail.com>
2023-02-09 20:30:35 +0100

    Needs testing on PC with PMBM data

--- ravens_parser_parallell_multicluster.py
+++ ravens_parser_parallell_multicluster.py
@@ -28,74 +28,29 @@ def loop_func(pmbm_file):
     pmbm_file_path = Path(pmbm_file)
     pmbm_filename = pmbm_file_path.name[:-len(pmbm_file_path.suffix)]
 
-    path = Path(f"{OUTPUT_PATH_BASE}/{pmbm_filename}")
+    path = Path(f"{OUTPUT_PATH_BASE}")
     path.mkdir(parents=True, exist_ok=True)
     
+    save_path = f"{path}/{pmbm_filename}_stats"
+
     num_clusters = len(prior_hypotheses_per_cluster)
     if num_clusters == 0:
         return
 
-    digits = int(np.ceil(np.log10(num_clusters)))
+    mcmhlbp = py_dfg_da.lbp.lbp_multicluster(R, prior_hypotheses_per_cluster)
+    mcmhlbp_marginals = mcmhlbp.track_association_marginals()
+    bethe_normalization_constant = mcmhlbp.bethe_pseudodual_normalization_constant()
+    exact_marginals, exact_normalization_constant = py_dfg_da.factor_graph.exact_marginals_and_normalization_constant(R, prior_hypotheses_per_cluster)
 
-    for k, prior_hypotheses in enumerate(prior_hypotheses_per_cluster):
-        cluster_stats = sl.ClusterData()
-        save_path = f"{path}/cluster{str(k).zfill(digits)}"
-        tracks_in_cluster = frozenset(tt for t,_ in prior_hypotheses for tt in t)
-        t_idx = np.sort(np.fromiter(tracks_in_cluster, dtype=int)) - 1
+    cluster_data = sl.MulticlusterData(
+        exact_marginals=exact_marginals.T,
+        exact_normalization_constant=exact_normalization_constant,
+        mhlbp_marginals=mcmhlbp_marginals.T,
+        bethe_normalization_constant=bethe_normalization_constant
+    )
 
-        try:
-            exact_marginals, (exact_normalization_constants,) = exact_marginal_computer(R_LC, prior_hypotheses)
-            exact_stats = sl.ExactStats(
-                marginals=sl.Marginals(exact_marginals[t_idx, :]),
-                normalization_constants=exact_normalization_constants
-            )
-            cluster_stats.exact_stats = exact_stats
-            cluster_stats.explicit_hypothesis_enumeration_error = False
-        except ExplicitHypothesisEnumerationError:
-            cluster_stats.explicit_hypothesis_enumeration_error = True
-            exact_normalization_constants = None
+    cluster_data.save_data(save_path)
 
-        lbp_williams_marginals, (approx_normalization_constants, williams_iters, williams_converged_list, lbp_williams_marginals_exact_norm_const) = approx_marginal_computers["lbp_williams"](
-            R_LC, prior_hypotheses, 
-            own_normalizing_constants=exact_normalization_constants
-        )
-        if lbp_williams_marginals_exact_norm_const is not None:
-            lbp_williams_marginals_exact_norm_const = sl.Marginals(lbp_williams_marginals_exact_norm_const[t_idx, :])
-
-        lbp_mh_marginals, (tot_iters, msg_iters, lbp_converged) = approx_marginal_computers["lbp_mh"](R_LC, prior_hypotheses)
-
-        lbp_marginals_bethe, bethe_constants_odin, bethe_constants_lc = approx_marginal_computers["lbp_bethe"](R_LC, prior_hypotheses)
-
-        lbp_stats = sl.LBPStats(
-            num_iters_msg=msg_iters,
-            num_iters=tot_iters,
-            marginals=sl.Marginals(lbp_mh_marginals[t_idx, :]),
-            converged=lbp_converged
-        )
-        williams_stats = sl.WilliamsStats(
-            lbp_iters=williams_iters,
-            marginals=sl.Marginals(lbp_williams_marginals[t_idx, :]),
-            marginals_exact_normalization_constant=lbp_williams_marginals_exact_norm_const,
-            normalization_constants=approx_normalization_constants,
-            converged_list=williams_converged_list
-        )
-
-        bethe_stats = sl.BetheStats(
-            marginals=sl.Marginals(lbp_marginals_bethe[t_idx, :]),
-            normalization_constants_odin=bethe_constants_odin,
-            normalization_constants_lc=bethe_constants_lc
-        )
-
-        cluster_stats.lbp_stats = lbp_stats
-        cluster_stats.williams_stats = williams_stats
-        cluster_stats.bethe_stats = bethe_stats
-
-        cluster_stats.cardinality = len(tracks_in_cluster)
-
-        cluster_stats.tracks = tracks_in_cluster
-        cluster_stats.num_hypotheses = len(prior_hypotheses)
-
-        cluster_stats.save_data(save_path)
 
 
 if __name__ == "__main__":
