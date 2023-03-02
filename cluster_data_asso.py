45126563fc7a814a8b49d311aecdfeae25f6e612
Odin <odin.sev@gmail.com>
2023-03-02 02:01:36 +0100

    Working hard-coded example of how to avoid cluster merging for normalization constant

--- cluster_data_asso.py
+++ cluster_data_asso.py
@@ -13,6 +13,29 @@ def cluster_reward_matrix(R, tracks_in_cluster):
     return np.hstack((Rd, Rmd))
 
 
+def remove_track_from_reward_mat(R, track):
+    n = R.shape[0]
+    idxs = np.arange(n) + 1
+    idxs = np.delete(idxs, track - 1)
+    return cluster_reward_matrix(R, idxs)
+
+
+def edmund_to_lc(R):
+    n, mpn = R.shape
+    m = mpn - n
+    Rd = R[:, :m]
+    Rmd = np.diag(R[:, m:])
+    return np.hstack((Rmd[:, None], Rd))
+
+
+def lc_to_edmund(R_LC):
+    Rmd = R_LC[:, 0]
+    Rd = R_LC[:, 1:]
+    n = len(Rmd)
+    Rmd_temp = np.full((n, n), -np.inf)
+    Rmd_temp[np.diag_indices(n)] = Rmd
+    return np.hstack((Rd, Rmd_temp))
+
 
 if __name__ == "__main__":
     R = np.array([
@@ -41,14 +64,15 @@ if __name__ == "__main__":
 
     prior_hypotheses_posterior = prior_hypotheses_per_cluster[0].combine(prior_hypotheses_per_cluster[1])
 
-    for k, h in enumerate(prior_hypotheses_posterior):
-        ts = np.array(h.tracks())
-        t_idxs = ts - 1
-        print(f"Hypothesis {k+1}: {ts}")
-        hypos = np.array([pdd.hypothesis.mo_to_to_hypothesis(hh, n) for hh in pdd.hypothesis.hypothesis_enumeration(R, h)])
-        print(f" {ts}")
-        print(hypos[:, t_idxs])
+    # for k, h in enumerate(prior_hypotheses_posterior):
+    #     ts = np.array(h.tracks())
+    #     t_idxs = ts - 1
+    #     print(f"Hypothesis {k+1}: {ts}")
+    #     hypos = np.array([pdd.hypothesis.mo_to_to_hypothesis(hh, n) for hh in pdd.hypothesis.hypothesis_enumeration(R, h)])
+    #     print(f" {ts}")
+    #     print(hypos[:, t_idxs])
 
+    np.set_printoptions(suppress=True)
 
     exact_margs, exact_norm = pdd.hypothesis.association_marginal_posteriors_normalization_constant(R, prior_hypotheses_posterior)
     print(exact_margs)
@@ -96,11 +120,16 @@ if __name__ == "__main__":
             hypos = np.array([pdd.hypothesis.mo_to_to_hypothesis(hh, n) for hh in pdd.hypothesis.hypothesis_enumeration(Rc1, h)])
             if existing:
                 valid_hypos = np.where(hypos[:, 2] == a3)[0]
-                hypos[valid_hypos, :]
+                hypos = hypos[valid_hypos, :]
 
-            scores = np.array([
-                pdd.hypothesis.prior_hypothesis_conditional_association_probability(hh, h, Rc1) for hh in hypos
-            ])
+            # Get the reward from the two remaining tracks. This should infact hold for all a3...
+            Rlc_c1 = edmund_to_lc(Rc1)
+            existing_tracks = h.tracks()
+            if 3 in existing_tracks:
+                existing_tracks.remove(3)
+            existing_tracks = np.array(existing_tracks) - 1
+            hypos = hypos[:, existing_tracks]
+            scores = [np.sum(Rlc_c1[existing_tracks, a]) for a in hypos]
 
             Zc1 += (np.exp(scores)*prior_prob).sum()
 
@@ -112,23 +141,30 @@ if __name__ == "__main__":
         # Loop over prior hypotheses in cluster 2.
         # Things are probably different here as the track is not contained in any of the prior hypotheses. Instead I assume that the logic is to 
         # Remove illegal associations to a measurement claimed by track 3? This logic should hold above
-        existing = 0 <= a3 <= m
+        if a3 == 0:
+            old2new = {
+                4: 1,
+                5: 2
+            }
+            ph_c2.reindex_tracks(old2new)
         for h in ph_c2:
-            contained_in_hypo = h.contains(3)
-            should_continue = (existing and not contained_in_hypo) or (not existing and contained_in_hypo)
-            if should_continue:
-                continue
-
             prior_prob = h.probability()
 
-            hypos = np.array([pdd.hypothesis.mo_to_to_hypothesis(hh, n) for hh in pdd.hypothesis.hypothesis_enumeration(Rc2, h)])
-            if existing:
-                valid_hypos = np.where(hypos[:, 2] == a3)[0]
-                hypos[valid_hypos, :]
+            # If a3 is associated with a measurement, we need to delete the hypotheses that associates tracks in the cluster to that measurement
+            if 1 <= a3 <= m:
+                legal_meas = np.arange(m)
+                legal_meas = np.delete(legal_meas, a3 - 1)
+                Rc2_temp = np.hstack((Rc2[:, legal_meas], Rc2[:, m:]))
+            else:
+                Rc2_temp = Rc2
 
-            scores = np.array([
-                pdd.hypothesis.prior_hypothesis_conditional_association_probability(hh, h, Rc1) for hh in hypos
-            ])
+            hypos = np.array([pdd.hypothesis.mo_to_to_hypothesis(hh, n) for hh in pdd.hypothesis.hypothesis_enumeration(Rc2_temp, h)])
+
+            existing_tracks = np.array(h.tracks()) - 1
+            hypos = hypos[:, existing_tracks]
+
+            Rlc_c2 = edmund_to_lc(Rc2_temp)
+            scores = [Rlc_c2[existing_tracks, a] for a in hypos]
 
             Zc2 += (np.exp(scores)*prior_prob).sum()
 
