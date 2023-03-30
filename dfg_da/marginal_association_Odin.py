Computing correct normalization constant, but wrong marginals in some cases...

dfg_da/marginal_association_Odin.py
@@ -976,8 +976,8 @@ def exact_marginal(llr: np.ndarray, do_cluster: bool = True, **kwargs) -> tuple[
     # the hypothesis.
     JPDA_hyp_mat = np.empty((n, Nhypotheses[0]), dtype=np.int16)
 
-    # Hypotheses probabilities #! SHOULD BE ZEROS??
-    hyp_prob_log = np.ones((Nhypotheses[0],))
+    # Hypotheses probabilities #! SHOULD BE ZEROS?? YES!!!!
+    hyp_prob_log = np.zeros((Nhypotheses[0],))
 
     # calculate "extended alphabet" association hypothesis, unnormalized probability.
     for i in range(n):
@@ -999,17 +999,15 @@ def exact_marginal(llr: np.ndarray, do_cluster: bool = True, **kwargs) -> tuple[
 
     # normalize and get probabilities
     loglikelihood = logsumexp(hyp_prob_log)  # TODO: Verify that this is ll
-    hyp_prob_log -= loglikelihood  # Normalize the probabilities
-    hypProb = np.exp(hyp_prob_log)
+    # hyp_prob_log -= loglikelihood  # Normalize the probabilities
+    hypProb = np.exp(hyp_prob_log - loglikelihood)
+    hyp_prob_log = hyp_prob_log[~unfeasible_hyps]
+
 
     # calculate the marginalization
     # marginal probability matrix: Rows as tracks and Columns as associations.
     JPDAprobs = (hypProb[None, None] * JPDA_hyp_mat_is_j).sum(axis=2).T
 
-    j_new_track_hyps = np.logical_not(np.any(JPDA_hyp_mat_is_j[1:], axis=1))
-    # array of probabilities for j not coming from existing track
-    notTrackProb = (hypProb[None] * j_new_track_hyps).sum(axis=1)
-
     # calculate number of hypotheses
     # Nhyp = (hypProbLog >= -np.inf).sum()
-    return JPDAprobs, notTrackProb, loglikelihood
+    return JPDAprobs, hyp_prob_log, loglikelihood

dfg_da/marginals_computers.py
@@ -296,7 +296,6 @@ class ClusterHypothesesPosterior:
         
         return updated_hypothesis_index_map_master
 
-
     def merge_clusters_labled(self, assocLocal: np.ndarray, prior_hypotheses_per_cluster: pdd.hypothesis.HypothesesList):
         # Initialize
         prior_hypotheses_per_cluster_posterior, hypothesis_index_map = self.create_master_mapping(assocLocal, prior_hypotheses_per_cluster)
@@ -330,6 +329,15 @@ class MulticlusterMarginalsComputer(ABC):
         return None
     
 
+@dataclass(frozen=True)
+class MulticlusterExactOutput:
+    exact_marginals: np.ndarray
+    hypo_cond_normalization_constants_per_cluster: List[np.ndarray]
+    normalization_constant_per_cluster: np.ndarray
+    exact_normalization_constant: float
+    cluster_hypotheses_posterior: ClusterHypothesesPosterior
+
+
 class MulticlusterExact(MulticlusterMarginalsComputer):
     def __call__(self, R_LC: np.ndarray, prior_hypotheses_per_cluster: pdd.hypothesis.HypothesesList, **kwargs) -> np.ndarray:
         return self.compute_marginals(R_LC, prior_hypotheses_per_cluster, **kwargs)
@@ -339,41 +347,72 @@ class MulticlusterExact(MulticlusterMarginalsComputer):
         if not "assocLocal" in kwargs:
             raise ValueError("assocLocal is required as input parameter because of cluster merging!")
 
+        # Start by merging clusters if necessary
         assocLocal = kwargs["assocLocal"]
+        cluster_hypotheses_posterior = ClusterHypothesesPosterior(assocLocal=assocLocal, prior_hypotheses_per_cluster = prior_hypotheses_per_cluster)
+        prior_hypotheses_per_cluster_posterior = cluster_hypotheses_posterior.prior_hypotheses_per_cluster_posterior
         n, mp1 = R_LC.shape
         m = mp1 - 1
         all_tracks_idx = np.arange(n)
 
-        
-
-        normalizing_constants = np.empty(len(prior_hypotheses))
+        # Initialize variables
+        hypo_cond_normalization_constants_per_cluster = []
+        normalization_constants_per_cluster = np.empty(len(prior_hypotheses_per_cluster_posterior))
         marginal_total = np.zeros((n, m + 1 + 1))
         conditioned_marginals = np.empty((n, m + 2))
+        _0 = np.zeros((n, m + 1))
+        _1 = np.ones((n, 1))
 
-        _0 = np.zeros((n, 1))
-        for k, (tracks, hypo_prob) in enumerate(prior_hypotheses):
-            R_sub = R_LC[tracks-1, :]
-            JPDAprobs, _, loglikelihood = exact_marginal(R_sub, False)
+        # Loop over clusters
+        for c, prior_hypotheses in enumerate(prior_hypotheses_per_cluster_posterior):
+            hypo_cond_normalizing_constants = np.empty(len(prior_hypotheses))
 
-            # We need to concatenate the JPDAprobs with all tracks and existence probs
-            existing_tracks_idx = tracks - 1
-            non_existing_tracks_idx = np.delete(all_tracks_idx, existing_tracks_idx)
+            print(f"Cluster {c+1}")
 
-            existing_probs = np.hstack((JPDAprobs, _0[:len(tracks)]))
-            nonexisting_probs = np.hstack((np.zeros((len(non_existing_tracks_idx), m + 1)), np.ones((len(non_existing_tracks_idx), 1))))
+            normalizing_constant_cluster = 0.0
+            for k, hypothesis in enumerate(prior_hypotheses):
+                existing_tracks_idx = np.array(hypothesis.tracks()) - 1
+                print(existing_tracks_idx+1)
+                log_prob = hypothesis.log_prob()
+                R_sub = R_LC[existing_tracks_idx, :]
+                JPDAprobs, hyp_prob_log, loglikelihood = exact_marginal(R_sub, False)
 
-            conditioned_marginals[existing_tracks_idx] = existing_probs
-            conditioned_marginals[non_existing_tracks_idx] =  nonexisting_probs
+                # We need to concatenate the JPDAprobs with all tracks and existence probs
+                non_existing_tracks_idx = np.delete(all_tracks_idx, existing_tracks_idx)
 
-            normalizing_constant = np.exp(loglikelihood)
+                existing_probs = np.hstack((JPDAprobs, _0[:len(existing_tracks_idx), 0, None]))
+                nonexisting_probs = np.hstack((_0[len(non_existing_tracks_idx)], _1[len(non_existing_tracks_idx), 0, None]))
 
-            normalizing_constants[k] = normalizing_constant
+                conditioned_marginals[existing_tracks_idx] = existing_probs
+                conditioned_marginals[non_existing_tracks_idx] =  nonexisting_probs
 
-            marginal_total += conditioned_marginals * normalizing_constant * hypo_prob
+                hypo_cond_normalization_constant = np.exp(loglikelihood)
+
+                hypo_cond_normalizing_constants[k] = hypo_cond_normalization_constant
+
+                marginal_total += conditioned_marginals * np.exp(loglikelihood + log_prob)
+
+                normalizing_constant_cluster += np.sum(np.exp(hyp_prob_log + log_prob))
+                
+
+            normalization_constants_per_cluster[c] = normalizing_constant_cluster
+            
+            hypo_cond_normalization_constants_per_cluster.append(hypo_cond_normalizing_constants)
+
+
+        exact_normalization_constant = np.prod(normalization_constants_per_cluster)
 
         marginal_total = marginal_total / marginal_total.sum(axis=1).reshape(-1, 1)
 
         assert (np.abs(marginal_total.sum(axis=1) - 1.0) < 1e-6).all()
         assert ((0 <= marginal_total) & (marginal_total <= 1.0)).all()
 
-        return marginal_total, (normalizing_constants,)
+        output = MulticlusterExactOutput(
+            exact_marginals=marginal_total,
+            hypo_cond_normalization_constants_per_cluster=hypo_cond_normalization_constants_per_cluster,
+            normalization_constant_per_cluster=normalization_constants_per_cluster,
+            exact_normalization_constant=exact_normalization_constant,
+            cluster_hypotheses_posterior=cluster_hypotheses_posterior
+        )
+
+        return output

test_pybinds.py
@@ -2,6 +2,7 @@ import py_dfg_da
 import numpy as np
 import dfg_da.marginal_association_Odin as ma
 import dfg_da.marginals_computers as mc
+import pickle
 
 if __name__ == "__main__":
     R = np.array([
@@ -55,16 +56,70 @@ if __name__ == "__main__":
 
     output = py_dfg_da.lbp.lbp_multicluster(R, prior_hypotheses_per_cluster)
     np.set_printoptions(suppress=True)
-    print("LBP")
-    print(output.track_association_marginals().T)
-    print(output.bethe_pseudodual_normalization_constant())
+    # print("LBP")
+    # print(output.track_association_marginals().T)
+    # print(output.bethe_pseudodual_normalization_constant())
 
     merged_hypos = prior_hypotheses_per_cluster[0].combine(prior_hypotheses_per_cluster[1])
     exact_margs, exact_norm = py_dfg_da.hypothesis.association_marginal_posteriors_normalization_constant(R, merged_hypos)
 
+    margs, const = py_dfg_da.factor_graph.exact_marginals_and_normalization_constant(R, prior_hypotheses_per_cluster)
+    # print(margs.T)
+    # print(const)
+
+    # print("\nExact")
+    # print(exact_margs.T)
+    # print(exact_norm)
+
+
+    R = np.array([
+        [    3.0, -np.inf,  -np.inf,   -0.60, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf],
+        [    3.2, -np.inf,  -np.inf, -np.inf,   -0.56, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf],
+        [   -3.0,     1.2,  -np.inf, -np.inf, -np.inf,   -0.46, -np.inf, -np.inf, -np.inf, -np.inf],
+        [-np.inf, -np.inf,      1.7, -np.inf, -np.inf, -np.inf,   -0.46, -np.inf, -np.inf, -np.inf],
+        [-np.inf, -np.inf,      2.3, -np.inf, -np.inf, -np.inf, -np.inf,   -0.46, -np.inf, -np.inf],
+        [-np.inf,     3.0,  -np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf,   -0.62, -np.inf],
+        [-np.inf,    -0.4,  -np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf, -np.inf,   -0.55],
+    ], order='F')
+
+
+    prior_hypotheses_per_cluster: py_dfg_da.hypothesis.HypothesesList = py_dfg_da.hypothesis.HypothesesList([
+        py_dfg_da.hypothesis.Hypotheses([
+            py_dfg_da.hypothesis.Hypothesis([1, 2], np.log(0.5)),
+            py_dfg_da.hypothesis.Hypothesis([1, 3], np.log(0.5))
+        ]),
+        py_dfg_da.hypothesis.Hypotheses([
+            py_dfg_da.hypothesis.Hypothesis([4], np.log(0.5)),
+            py_dfg_da.hypothesis.Hypothesis([5], np.log(0.5)),
+        ]),
+        py_dfg_da.hypothesis.Hypotheses([
+            py_dfg_da.hypothesis.Hypothesis([6], np.log(0.2)),
+            py_dfg_da.hypothesis.Hypothesis([7], np.log(0.8)),
+        ]),
+    ])
+
+    margs, const = py_dfg_da.factor_graph.exact_marginals_and_normalization_constant(R, prior_hypotheses_per_cluster)
     print("\nExact")
-    print(exact_margs.T)
-    print(exact_norm)
+    print(margs.T)
+    print(const)
+
+
+    # path = "./test_cpp_pickle"
+    # with open(path, "wb") as f:
+    #     pickle.dump(output, f)
+
+    # with open(path, "rb") as f:
+    #     mhlbp_from_pickle = pickle.load(f)
+
+    # print("LBP from pickle")
+    # print(mhlbp_from_pickle.track_association_marginals().T)
+    # print(mhlbp_from_pickle.bethe_pseudodual_loglikelihood())
+    # print(mhlbp_from_pickle.bethe_pseudodual_normalization_constant())
+    # for data in mhlbp_from_pickle.cluster_data:
+    #     print(data.phi_vec)
+    #     print(data.t_idx)
+    #     print(data.t2h_not)
+    #     print(data.t2h)
 
     # print("Multicluster!!!")
     # exact_margs_mc, exact_norm_mc = py_dfg_da.hypothesis.association_marginal_posteriors_normalization_constant_multicluster(R, prior_hypotheses_per_cluster)